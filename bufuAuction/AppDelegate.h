d17c180a52159d021587bc6c39c4a5ef8ded231f
JieLi <zmctinas@163.com>
2018-07-18 14:25:44 +0800

    Initial Commit

---
+++ bufuAuction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bufuAuction
+//
+//  Created by Andy on 2018/7/18.
+//  Copyright © 2018年 JieLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bufuAuction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bufuAuction
+//
+//  Created by Andy on 2018/7/18.
+//  Copyright © 2018年 JieLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
